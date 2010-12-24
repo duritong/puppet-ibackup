54335ee89c406b7430984d3cb19c4a0ca95a726d
mh <mh@immerda.ch>
2010-12-24 09:38:30 +0100

    intendation

--- manifests/simplebackup.pp
+++ manifests/simplebackup.pp
@@ -8,64 +8,64 @@ class ibackup::simplebackup(
   $ssh_key_basepath = "/etc/puppet/modules/site-securefile/files",
   $disk_target = "/srv/backups"
 ) {
-    include ibackup::simpledisks
+  include ibackup::simpledisks
 
-    include rsync::client
-    if $use_shorewall {
-        include shorewall::rules::out::ibackup
-    }
+  include rsync::client
+  if $use_shorewall {
+    include shorewall::rules::out::ibackup
+  }
 
-    file{'/e/backup/bin/ext_backup':
-        source => [ "puppet:///modules/site-ibackup/scripts/${fqdn}/ext_backup",
-                    "puppet:///modules/site-ibackup/scripts/${type}/ext_backup" ],
-        owner => root, group => 0, mode => 0700;
-    }
-    file{'/e/backup/bin/ext_backup.sh':
-      ensure => absent,
-    }
+  file{'/e/backup/bin/ext_backup':
+    source => [ "puppet:///modules/site-ibackup/scripts/${fqdn}/ext_backup",
+                "puppet:///modules/site-ibackup/scripts/${type}/ext_backup" ],
+    owner => root, group => 0, mode => 0700;
+  }
+  file{'/e/backup/bin/ext_backup.sh':
+    ensure => absent,
+  }
 
-    file{'/e/backup/bin/ext_backup.config':
-        source => [ "puppet:///modules/site-ibackup/scripts/${fqdn}/ext_backup.config",
-                    "puppet:///modules/site-ibackup/scripts/${type}/ext_backup.config",
-                    'puppet:///modules/site-ibackup/scripts/ext_backup.config' ],
-        owner => root, group => 0, mode => 0600;
-    }
+  file{'/e/backup/bin/ext_backup.config':
+    source => [ "puppet:///modules/site-ibackup/scripts/${fqdn}/ext_backup.config",
+                "puppet:///modules/site-ibackup/scripts/${type}/ext_backup.config",
+                'puppet:///modules/site-ibackup/scripts/ext_backup.config' ],
+    owner => root, group => 0, mode => 0600;
+  }
 
-    # this will generate the source for the deply and the public key for the disk
-    $ssh_keys = ssh_keygen("${$ssh_key_basepath}/backup/keys/${fqdn}/${backup_host}")
+  # this will generate the source for the deply and the public key for the disk
+  $ssh_keys = ssh_keygen("${$ssh_key_basepath}/backup/keys/${fqdn}/${backup_host}")
 
-    securefile::deploy { "${backup_host}_ssh_key":
-        source  => "backup/keys/${fqdn}/${backup_host}",
-        path    => "backup/keys/${backup_host}",
-        require => File['/e/backup/keys'],
-        owner => root, group => 0, mode => 0600;
-    }
+  securefile::deploy { "${backup_host}_ssh_key":
+    source  => "backup/keys/${fqdn}/${backup_host}",
+    path  => "backup/keys/${backup_host}",
+    require => File['/e/backup/keys'],
+    owner => root, group => 0, mode => 0600;
+  }
 
-    $public_key = split($ssh_keys[1],' ')
-    @@ibackup::target{$fqdn:
-      sshkey_type => $public_key[0],
-      sshkey => $public_key[1],
-      target => "$disk_target/${$fqdn}",
-      tag => $backup_host,
-    }
+  $public_key = split($ssh_keys[1],' ')
+  @@ibackup::target{$fqdn:
+    sshkey_type => $public_key[0],
+    sshkey => $public_key[1],
+    target => "$disk_target/${$fqdn}",
+    tag => $backup_host,
+  }
 
-    case $kernel {
-        default: {
-            file{'/etc/cron.daily/ext_backup':
-                ensure => '/e/backup/bin/ext_backup',
-                require => [ Securefile::Deploy["${backup_host}_ssh_key"], File['/e/backup/bin/ext_backup'] ],
-            }
-            file{'/etc/cron.daily/ext_backup.sh':
-              ensure => absent,
-            }
-        }
-        openbsd: {
-            cron { 'ibackup_job':
-                command => '/e/backup/bin/ext_backup',
-                minute => '15',
-                hour => '2',
-                require => [ Securefile::Deploy["${backup_host}_ssh_key"], File['/e/backup/bin/ext_backup'] ],
-            }  
-        }
+  case $kernel {
+    default: {
+      file{'/etc/cron.daily/ext_backup':
+        ensure => '/e/backup/bin/ext_backup',
+        require => [ Securefile::Deploy["${backup_host}_ssh_key"], File['/e/backup/bin/ext_backup'] ],
+      }
+      file{'/etc/cron.daily/ext_backup.sh':
+        ensure => absent,
+      }
     }
+    openbsd: {
+      cron { 'ibackup_job':
+        command => '/e/backup/bin/ext_backup',
+        minute => '15',
+        hour => '2',
+        require => [ Securefile::Deploy["${backup_host}_ssh_key"], File['/e/backup/bin/ext_backup'] ],
+      }  
+    }
+  }
 }
