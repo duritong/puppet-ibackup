647241b1b0ccdaaa43f4571305b7d9a10e993d1f
mh <mh@immerda.ch>
2011-11-23 00:47:01 +0100

    fix stupid typo

--- manifests/host.pp
+++ manifests/host.pp
@@ -12,7 +12,7 @@ class ibackup::host(
   Ibackup::Target<<| tag == $backup_domain |>>
 
   @@sshkey{$backup_domain:
-    type => 'ssh-rsa'',
+    type => 'ssh-rsa',
     key => $sshrsakey,
     ensure => present,
     tag => $backup_domain,
