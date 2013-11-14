de435770ad7ea4f4c7fa08d95a5f4c6fe67ebc67
mh <mh@immerda.ch>
2013-11-14 23:56:16 +0100

    lintify and remove target more easier

--- manifests/target.pp
+++ manifests/target.pp
@@ -6,59 +6,68 @@
 # target: On which directory the backup user has access.
 #         Required.
 define ibackup::target(
-    $ensure = 'present',
-    $sshkey = 'absent',
-    $sshkey_type = 'ssh-rsa',
-    $user_password = 'absent',
-    $user_password_crypted = true,
-    $target
+  $target,
+  $ensure                 = 'present',
+  $sshkey                 = 'absent',
+  $sshkey_type            = 'ssh-rsa',
+  $user_password          = 'absent',
+  $user_password_crypted  = true,
 ){
 
-    if ($ensure=='present'){
-      if ($sshkey=='absent') or ($user_password=='absent'){
-        fail("You must set \$sshkey and \$user_password on Ibackup::Target[${name}]!")
-      }
-    }
+  if ($ensure=='present') and
+    (($sshkey=='absent') or ($user_password=='absent')) {
+    fail("You must set \$sshkey & \$user_password on Ibackup::Target[${name}]!")
+  }
 
-    user::managed{$name:
-        ensure => $ensure,
-        groups => 'backup',
-        require => Group['backup'],
-        password => $user_password ? {
-            'trocla' => trocla("ibackup_${name}",'sha512crypt'),
-            default => $user_password
-        },
-        password_crypted => $user_password_crypted,
-    }
+  $password = $user_password ? {
+    'trocla'  => trocla("ibackup_${name}",'sha512crypt'),
+    default   => $user_password
+  }
+  user::managed{$name:
+    ensure            => $ensure,
+    groups            => 'backup',
+    require           => Group['backup'],
+    password          => $password,
+    password_crypted  => $user_password_crypted,
+  }
 
-    file{$target:
-        ensure => $ensure ? {
-          'present' => directory,
-          default => absent
-        },
-    }
+  $ensure_target = $ensure ? {
+    'present' => directory,
+    default   => absent
+  }
+  file{$target:
+      ensure => $ensure_target,
+  }
 
-    if ($ensure!='present'){
-      File[$target]{
-        purge => true,
-        force => true,
-        backup => false,
-        recurse => true,
-        before => User["$name"],
-      }
-    } else {
-      File[$target]{
-        require => User[$name],
-        owner => $name, group => 0, mode => 0600,
-      }
-      sshd::ssh_authorized_key{"backupkey_${name}":
-        ensure => $ensure,
-        key => $sshkey,
-        type => $sshkey_type,
-        user => $name,
-        options => ["command=\"/usr/local/bin/rrsync ${target}\"",'no-pty','no-X11-forwarding','no-agent-forwarding','no-port-forwarding'],
- 
-        require => File["$target"],
-      }
+  if ($ensure != 'present'){
+    exec{"remove_${target}":
+      command => "rm -rf ${target}",
+      onlyif  => "test -d ${target}",
+      before  => File[$target],
     }
+    File[$target]{
+      purge   => true,
+      force   => true,
+      backup  => false,
+      recurse => true,
+      before  => User[$name],
+    }
+  } else {
+    File[$target]{
+      require => User[$name],
+      owner   => $name,
+      group   => 0,
+      mode    => '0600',
+    }
+    sshd::ssh_authorized_key{"backupkey_${name}":
+      ensure  => $ensure,
+      key     => $sshkey,
+      type    => $sshkey_type,
+      user    => $name,
+      options => ["command=\"/usr/local/bin/rrsync ${target}\"",
+        'no-pty','no-X11-forwarding','no-agent-forwarding',
+        'no-port-forwarding'],
+      require => File[$target],
+    }
+  }
 }
