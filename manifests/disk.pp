144cdc350f08e73ebb3d2d7d333d7afcd2dc2227
mh <mh@immerda.ch>
2015-11-15 18:28:13 +0100

    modernize disk define

--- manifests/disk.pp
+++ manifests/disk.pp
@@ -1,20 +1,23 @@
+# a simple disk aka. mountpoint
 define ibackup::disk(
-    $device,
-    $fstype = 'ext3',
-    $options = 'noatime,nodev,noexec'
+  $device,
+  $fstype  = 'ext4',
+  $options = 'noatime,nodev,noexec'
 ){
-    include ibackup::disks
+  include ::ibackup::disks
 
-    file{"/data/backup_${name}":
-        ensure => directory,
-        owner => root, group => 0, mode => 0755;
-    }
+  file{"/data/backup_${name}":
+    ensure => directory,
+    owner  => root,
+    group  => 0,
+    mode   => '0755';
+  }
 
-    mount{"/data/backup_${name}":
-        device => $device,
-        ensure => mounted,
-        fstype => $fstype,
-        options => $options,
-        require => File["/data/backup_${name}"],
-    }
+  mount{"/data/backup_${name}":
+    ensure  => mounted,
+    device  => $device,
+    fstype  => $fstype,
+    options => $options,
+    require => File["/data/backup_${name}"],
+  }
 }
