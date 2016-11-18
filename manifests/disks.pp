53f170193ee0b3b09a612a628a1129954dd6c29e
mh <mh@immerda.ch>
2016-11-18 16:29:12 +0100

    linting for new parser

--- manifests/disks.pp
+++ manifests/disks.pp
@@ -1,8 +1,10 @@
 # manifests/disks.pp
 
 class ibackup::disks {
-    file{'/data':
-        ensure => directory,
-        owner => root, group => 0, mode => 0755;
-    }
+  file{'/data':
+    ensure => directory,
+    owner  => root,
+    group  => 0,
+    mode   => '0755';
+  }
 }
